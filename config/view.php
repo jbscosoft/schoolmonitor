876927bb2d7f128bee4b14aba6f262bcbc0791d7
jbscosoft <johnkalungi1@gmail.com>
2018-08-15 08:47:04 +0300

    c

--- config/view.php
+++ config/view.php
@@ -2906,7 +2906,9 @@
                </table>
             </div>
             <div class="col-md-2">
-               <a href="<?php echo '?token='.base64_encode('add_programme') ?>"  class="btn  btn-success btn-flat " style='margin-top:3%;'><i class="fa fa-plus"></i>&nbsp;Add Programme</a>
+                <button type="button" class="btn  btn-success btn-flat " style='margin-top:3%;' data-toggle="modal" data-target="#modal-default">
+                Add Programme
+              </button>
             </div>
             <!-- /.box-body -->
          </div>
@@ -3597,5 +3599,28 @@
 <!-- /.content -->
 <?php
    }
-
+function add_program(){
+    ?>
+    <div class="modal fade" id="modal-default">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">Default Modal</h4>
+        </div>
+        <div class="modal-body">
+          <p>One fine body&hellip;</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+  <?php
+}
    ?>
--- pages/dash.php
+++ pages/dash.php
@@ -520,5 +520,66 @@ ob_start();
     })
   })
 </script>
+<div class="modal fade" id="modal-default">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">New Programme</h4>
+        </div>
+        <div class="modal-body">
+        <form class="form-horizontal">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Programme Name</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" placeholder="e.g - Bachelor of Computer Science">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Code</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputPassword3" >
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Department</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputEmail3">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Level</label>
+                  <div class="col-sm-10">
+                  <select class="form-control select2 select2-hidden-accessible" style="width: 100%;" tabindex="-1" aria-hidden="true">
+                           <option selected="selected">--Select--</option>
+                           <option>PHD</option>
+                           <option>Masters</option>
+                           <option>Degree</option>
+                           <option>Diploma</option>
+                           <option>Short Course</option>
+                        </select>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Duration</label>
+                  <div class="col-sm-10">
+                    <input type="number" class="form-control" id="inputPassword3" >
+                  </div>
+                </div>
+              
+              </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
 </body>
 </html>
